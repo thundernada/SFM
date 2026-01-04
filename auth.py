1b73fb0d4f4a59e4fa4a8c8f511792f4e726afb8
thundernada <arci.mohamednada@hotmail.com>
2026-01-04 16:32:30 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,23 @@
+import streamlit as st
+
+# تغيير الباسورد من هنا فقط
 PROJECT_PASSWORD = "EGISF2026"
+
+def check_password():
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+
+    if not st.session_state.authenticated:
+        st.title("🔒 Secure Project Access")
+        password = st.text_input("Enter Project Password", type="password")
+
+        if st.button("Login"):
+            if password == PROJECT_PASSWORD:
+                st.session_state.authenticated = True
+                st.experimental_rerun()
+            else:
+                st.error("Incorrect password")
+
+        return False
+
+    return True
